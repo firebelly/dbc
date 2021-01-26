3e34bb404daf63b6a6c641b77849e07fccd90725
poopsplat <matt.m.soria@gmail.com>
2021-01-26 09:41:16 -0600

    Updating fabfile

--- fabfile.py
+++ fabfile.py
@@ -4,7 +4,7 @@ import os
 env.hosts = ['firebelly.co']
 env.user = 'firebelly'
 env.path = '~/Sites/dbc'
-env.remotepath = '/home/firebelly/webapps/dbc'
+env.remotepath = '/home/firebelly/apps/dbc'
 env.git_branch = 'master'
 env.warn_only = True
 env.remote_protocol = 'http'
@@ -17,4 +17,4 @@ def update():
     run('git pull origin {0}'.format(env.git_branch))
 
 def copy():
-  put('_site', '/home/firebelly/webapps/dbc/')
+  put('_site', '/home/firebelly/apps/dbc/')
